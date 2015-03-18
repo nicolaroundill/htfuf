b41db7eeb7128322ac3ef3f7557b3a5e1d14e1db
Guest User <Guest@f-05-06-lab.mddn.vuw.ac.nz>
2015-03-18 17:33:45 +1300

    Added comments to my javascript content so I can remember what I'm doing later

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//variable for scrollChange01 - changing the potato image
 var imageSet = 1;
 var scrollCount = 0;
 
@@ -14,7 +15,9 @@ window.onload = function(){
 //Parallax knife function
 function parallax() {
 
+	//get the information of this parallax id
 	var parallax1 = document.getElementById('parallax1');
+	//change the information of this id to be scroll position divided by 4
 	parallax1.style.top = (window.pageYOffset / 4) + 'px';
 	var parallax2 = document.getElementById('parallax2');
 	parallax2.style.top = (window.pageYOffset / 8) + 'px';
@@ -29,43 +32,40 @@ function parallax() {
 
 }
 
-//Function to change images when scrolling
+//Functions to change images when scrolling
+
+//function to change potatoes when scrolling
 function scrollChange01(){
+	//find position on screen from scrolling
 	var yImageOne = window.scrollY;
+	//take all info from "potato-img" id, assigned to the container the potato section is in
 	var imageSource = document.getElementById("potato-img");
-	//var part02 = document.getElementById("part02");
+	//so you can change the function to begin earlier
 	var yPadding = 200;
+	//finds the position of the top of the potato section/container
 	var part02y = $("#part02").offset().top;
 
-
+	//log scrolls to make sure it's working
 	console.log(scrollCount);
 
+	//if the current scroll position is greater than the beginning of the content container 
+	//(minus 200), AND the current scroll position is greater than this BIG number;
 	if (yImageOne > part02y - yPadding && yImageOne < 100000000){
+		//and if current scroll position is divisible by 10;
 		if (yImageOne % 10 === 0) {
+			//AND if this variable is less than 6 (because I only have 5 images to choose from), then:
 			if (imageSet < 6){
+				//change the image source to the next image
 				imageSource.src = "src/img/potatoes0" + imageSet + ".png";
+				//add a number to the image source
 				imageSet ++;
+				//console log that mofooooooo
 				console.log(imageSet);
 			}
 		}
 	}
 
-/*
-	//when YOffset is between beginning point and end point 
-	if (yImageOne > 100 && yImageOne < 10000){
-		//for every scroll that is divisible by 50
-		for (imageSet; imageSet < 5; imageSet++){
-			if (yImageOne % 50 === 0) {
-				//change variable imageSet to imageSet + 1
-				console.log(imageSet);
-				//change image source to "src/img/timer0" + imageSet + ".png"
-				imageSource.src = "src/img/potatoes0" + imageSet + ".png";
-			}
-			
-		}
 
-	}
-*/
 	
 	
 	
